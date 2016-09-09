05c8ea94f96eb9ad93ad2ed72138e51230db9953
Irpan Budiana <64187@kereta-api.co.id>
2016-09-09 13:52:39 +0700

    splash fadeout

--- app/Application.js
+++ app/Application.js
@@ -17,9 +17,28 @@ Ext.define('Movierent.Application', {
 
     init: function () {
         splashscreen = Ext.getBody().mask('Sedang memuat aplikasi...', 'splashscreen');
+
+        Ext.DomHelper.insertFirst(Ext.query('.x-mask-msg')[0], {
+            cls: 'x-splash-icon'
+        });
     },
     
     launch: function() {
-        console.log('launch');
+        var task = new Ext.util.DelayedTask(function () {
+
+            //Fade out the body mask
+            splashscreen.fadeOut({
+                duration: 1000,
+                remove:true
+            });
+
+            splashscreen.next().fadeOut({
+                duration: 1000,
+                remove: true
+            });
+
+            console.log('buka halaman');
+        });
+        task.delay(2000);
     }
 });
