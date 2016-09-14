66a3ab68306fd5e901e007782eff92f08896dcf2
Irpan Budiana <64187@kereta-api.co.id>
2016-09-14 10:01:22 +0700

    disable dulu splashscreen

--- app/Application.js
+++ app/Application.js
@@ -24,37 +24,38 @@ Ext.define('Movierent.Application', {
     ],
 
     init: function () {
-        splashscreen = Ext.getBody().mask('Sedang memuat aplikasi...', 'splashscreen');
-
-        splashscreen.addCls('splashscreen');
-
-        Ext.DomHelper.insertFirst(Ext.query('.x-mask-msg')[0], {
-            cls: 'x-splash-icon'
-        });
+        // splashscreen = Ext.getBody().mask('Sedang memuat aplikasi...', 'splashscreen');
+        //
+        // splashscreen.addCls('splashscreen');
+        //
+        // Ext.DomHelper.insertFirst(Ext.query('.x-mask-msg')[0], {
+        //     cls: 'x-splash-icon'
+        // });
     },
     
     launch: function() {
         Ext.tip.QuickTipManager.init();
-        var task = new Ext.util.DelayedTask(function () {
 
-            //Fade out the body mask
-            splashscreen.fadeOut({
-                duration: 1000,
-                remove:true
-            });
-
-            splashscreen.next().fadeOut({
-                duration: 1000,
-                remove: true,
-                listeners: {
-                    afteranimate: function(el, startTime, eOpts ){
+        // var task = new Ext.util.DelayedTask(function () {
+        //
+        //     //Fade out the body mask
+        //     splashscreen.fadeOut({
+        //         duration: 1000,
+        //         remove:true
+        //     });
+        //
+        //     splashscreen.next().fadeOut({
+        //         duration: 1000,
+        //         remove: true,
+        //         listeners: {
+        //             afteranimate: function(el, startTime, eOpts ){
                         Ext.widget('login');
-                    }
-                }
-            });
-
-            console.log('buka halaman');
-        });
-        task.delay(2000);
+        //             }
+        //         }
+        //     });
+        //
+        //     console.log('buka halaman');
+        // });
+        // task.delay(2000);
     }
 });
