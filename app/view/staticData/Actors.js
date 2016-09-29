2d981c49761e8571e5f4863ce0513df35b2c171e
Irpan Budiana <64187@kereta-api.co.id>
2016-09-29 16:09:11 +0700

    add grid actor view

---
+++ app/view/staticData/Actors.js
@@ -0,0 +1,37 @@
+Ext.define('Movierent.view.staticData.Actors', {
+    extend: 'Movierent.view.staticData.AbstractGrid',
+    alias: 'widget.actorsgrid',
+
+    store: 'staticData.Actors',
+
+    columns: [{
+        text: 'Actor Id',
+        width: 100,
+        dataIndex: 'actor_id',
+        filter: {
+            type: 'numeric'
+        }
+    }, {
+        text: 'First Name',
+        flex: 1,
+        dataIndex: 'first_name',
+        editor: {
+            allowBlank: false,
+            maxLength: 45
+        },
+        filter: {
+            type: 'string'
+        }
+    }, {
+        text: 'Last Name',
+        width: 200,
+        dataIndex: 'last_name',
+        editor: {
+            allowBlank: false,
+            maxLength: 45
+        },
+        filter: {
+            type: 'string'
+        }
+    }]
+});
